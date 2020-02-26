e4a1137748a5a54f82f9e9d4bd86a200b428359d
Adereglazov <sanya95@yandex.ru>
2020-02-26 13:07:02 +0300

    add animation for svg, add validation for email field

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,19 @@
-var submitButton = document.querySelector('.form__button');
-var emailField = document.querySelector('.form__text-field--letter');
+let submitButton = document.querySelector('.form__button');
+let emailField = document.querySelector('.form__text-field--email');
+let errorText = document.querySelector('.form__error-text');
 
-emailField.required = false;
+emailField.onblur = function () {
+	validEmail(emailField, errorText)
+};
+
+emailField.addEventListener('input', function () {
+	emailField.classList.remove('form__text-field--error');
+	errorText.innerHTML = '';
+});
 
 submitButton.addEventListener('click', function (evt) {
 
-	if (!emailField.value) {
+	if (!validEmail(emailField, errorText)) {
 		evt.preventDefault();
 
 		submitButton.classList.remove('form__animation');
@@ -17,3 +25,23 @@ submitButton.addEventListener('click', function (evt) {
 		submitButton.classList.remove('form__button--error');
 	}
 });
+
+function validEmail(email, errorText) {
+	let regularString = /^[\w-.]+@[\w-]+\.[a-z]{2,4}$/i;
+	let isValid = regularString.test(email.value);
+	let errorMessage = '';
+
+	emailField.classList.add('form__text-field--error');
+
+	if (!email.value) {
+		errorMessage = 'Введите адрес';
+	} else if (!isValid) {
+		errorMessage = 'Адрес введен неправильно!';
+	} else {
+		emailField.classList.remove('form__text-field--error');
+	}
+
+	errorText.innerHTML = errorMessage;
+
+	return errorMessage === '';
+}
