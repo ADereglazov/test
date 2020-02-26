93280331c752f4c5212ed42090b8531d1c6464ec
Adereglazov <sanya95@yandex.ru>
2020-02-26 22:47:18 +0300

    validation - alpha version, refactoring svg animation

--- source/js/script.js
+++ source/js/script.js
@@ -1,25 +1,42 @@
 let submitButton = document.querySelector('.form__button');
 let emailField = document.querySelector('.form__text-field--email');
-let passwordField = document.querySelector('.form__text-field--password');
 let errorTextEmail = document.querySelector('.form__error-email');
+let passwordField = document.querySelector('.form__text-field--password');
 let errorTextPassword = document.querySelector('.form__error-password');
+let confirmPasswordField = document.querySelector('.form__text-field--confirm-password');
+let errorTextConfirmPassword = document.querySelector('.form__error-confirm-password');
 
 emailField.onblur = function () {
-	isValidEmail(emailField, errorTextEmail)
+	isValidEmail(emailField, errorTextEmail);
 };
 
 emailField.addEventListener('input', function () {
-	emailField.classList.remove('form__text-field--error');
-	errorTextEmail.innerHTML = '';
+	removeErrorState(emailField, errorTextEmail);
 });
 
 passwordField.onblur = function () {
-	isValidPassword(passwordField, errorTextPassword)
+	isValidPassword(passwordField, errorTextPassword);
 };
 
 passwordField.addEventListener('input', function () {
-	passwordField.classList.remove('form__text-field--error');
-	errorTextPassword.innerHTML = '';
+	removeErrorState(passwordField, errorTextPassword);
+});
+
+confirmPasswordField.onblur = function () {
+	let errorMessage = '';
+
+	if (passwordField.value === confirmPasswordField.value) {
+		confirmPasswordField.classList.remove('form__text-field--error');
+	} else {
+		confirmPasswordField.classList.add('form__text-field--error');
+		errorMessage = 'Пароли не совпадают!';
+	}
+
+	errorTextConfirmPassword.innerHTML = errorMessage;
+};
+
+confirmPasswordField.addEventListener('input', function () {
+	removeErrorState(confirmPasswordField, errorTextConfirmPassword);
 });
 
 submitButton.addEventListener('click', function (evt) {
@@ -29,11 +46,9 @@ submitButton.addEventListener('click', function (evt) {
 	if (!isValidEmail(emailField, errorTextEmail)) {
 		modSubmitButton(evt);
 		emailField.focus();
-
 	} else if (!isValidPassword(passwordField, errorTextPassword)) {
 		modSubmitButton(evt);
 		passwordField.focus();
-
 	}
 	else {
 		submitButton.classList.remove('form__button--error');
@@ -47,8 +62,13 @@ function modSubmitButton(evt) {
 	submitButton.classList.add('form__button--error');
 }
 
+function removeErrorState(inputField, errorTextElement) {
+	inputField.classList.remove('form__text-field--error');
+	errorTextElement.innerHTML = '';
+}
+
 function isValidEmail(email, errorTextEmail) {
-	let regularString = /^[\w-.]+@[\w-]+\.[a-z]{2,4}$/i;
+	let regularString = /^[\w-.]+@[\w-]+\.[a-z]{2,4}$/;
 	let isValid = regularString.test(email.value);
 	let errorMessage = '';
 
